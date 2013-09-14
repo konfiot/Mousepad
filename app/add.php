9c0df55474a83dc33e6d88a15a8c6fc7e149bc0a
Arthur <spam.00456@gmail.com>
2013-09-14 15:26:28 -0400

    Commencage d'implémentation de l'API

---
+++ app/add.php
@@ -0,0 +1,26 @@
+<?php
+if (isset($_POST["data"])){
+    $data["sent"] = json_decode($_POST["data"]);
+    if (isset($data["sent"]["type"])){
+        switch ($data["sent"]["type"]){
+            case "todo" : 
+                $result = add_todo($data);
+            break;
+            case "note" : 
+                $result = add_note($data);
+            break;
+            case "file" :
+                $result = add_file($data);
+            break;
+            case "snippet" :
+                $result = add_snippet($data);
+            break;
+            case "checklist" :
+                $result = add_checklist($data);
+            break;
+        }
+    }
+    echo json_encode($result);
+}
+
+?>
