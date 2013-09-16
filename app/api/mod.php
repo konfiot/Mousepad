7a38ec923f8928073b7041f2fcc6a4024511d278
Arthur <spam.00456@gmail.com>
2013-09-16 13:06:05 -0400

    Ajout du fichier de fonction pour lister les cartes

---
+++ app/api/mod.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_POST["data"])){
+    $data["sent"] = json_decode($_POST["data"]);
+    if (isset($data["sent"]["type"])){
+        get($data);
+    } else {
+        $result = get_all();
+    }
+    echo json_encode($result);
+} else {
+    echo json_encode(array("error" => "unset_data"));
+}
+?>
