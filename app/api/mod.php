1d0b6d8a8a0d69f42c3ba83aa7eb7bb59dcd3792
Arthur <spam.00456@gmail.com>
2013-09-16 13:20:05 -0400

    remplissage de l'endpoint mod.php

--- app/api/mod.php
+++ app/api/mod.php
@@ -2,9 +2,25 @@
 if (isset($_POST["data"])){
     $data["sent"] = json_decode($_POST["data"]);
     if (isset($data["sent"]["type"])){
-        get($data);
-    } else {
-        $result = get_all();
+        switch ($data["sent"]["type"]){
+            case "todo" : 
+                $result = mod_todo($data);
+            break;
+            case "note" : 
+                $result = mod_note($data);
+            break;
+            case "file" :
+                $result = mod_file($data);
+            break;
+            case "snippet" :
+                $result = mod_snippet($data);
+            break;
+            case "checklist" :
+                $result = mod_checklist($data);
+            break;
+            default :
+                exit(json_encode(array("error" => "unknown_type")));
+        }
     }
     echo json_encode($result);
 } else {
