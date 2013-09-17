8bf052787a392d3535bb9d5566b3ff505f5c2a9e
Arthur <spam.00456@gmail.com>
2013-09-17 12:50:56 -0400

    Ajout de la fonction pour gérer l'envoi de fichiers

---
+++ app/functions/data/add/file.php
@@ -0,0 +1,17 @@
+<?php 
+function add_file($data) {
+    if ($data["type"] != "file"){
+        return json_encode({"error" : "expected file, but saw " . $data["type"]. " instead" });
+    }
+    
+    $title = htmlspecialchars($data["title"]);
+    $path = htmlspecialchars($data["path"]);
+    $parent = htmlspecialchars($data["parent"])
+    
+    $id = db_add_file($title, $path, $parent);
+    
+    if ($id === false){
+        return json_encode({"error" : "Database error" });
+    }
+}
+?>
