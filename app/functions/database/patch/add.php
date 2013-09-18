e1f83506cc58759429b99f006a80105b7050b474
Arthur <spam.00456@gmail.com>
2013-09-18 01:51:06 -0400

    Ajout de la fonction pour ajouter un patch à la DB

---
+++ app/functions/database/patch/add.php
@@ -0,0 +1,30 @@
+<?php
+define(DBTYPE, "json");
+define(JSONFILE, "diffs.json");
+
+function db_add_diff($diff, $parent){
+    switch (DBTYPE){
+        case "json" :
+            touch(JSONFILE);
+            
+            $file = fopen(JSONFILE, "w");
+            while (flock($file, LOCK_EX) === false);
+            $contents = fread($file, filesize(JSONTYPE));
+            
+            $uuid = uniqid()
+            $json = json_decode($contents);
+            $json[$uuid] = array("content" => $diff, "parent" => $parent, "timestamp" => time());
+            
+            fwrite($file, json_encode($json));
+            flock($file, LOCK_UN);
+            fclose($file);
+            
+            return $uuid
+
+        break;
+        default :
+            return false;
+        break;
+    }
+}
+?>
