1ac7dac6a0207401158e89f9acf46d72f5c8f242
Arthur <spam.00456@gmail.com>
2013-09-22 07:14:21 -0400

    Maintenant le titre est aussi sauvegradé

--- app/functions/data/mod/card.php
+++ app/functions/data/mod/card.php
@@ -1,17 +1,22 @@
 <?php 
 include "../functions/data/get.php";
 include "../functions/database/patch/add.php";
+include "../functions/database/card/set_title.php";
 include_once "../functions/diff_match_patch.php";
 
 
 function mod_card($data) {
     $content = htmlspecialchars($data["content"]);
-
+    $title = htmlspecialchars($data["title"]);
+    
     $old = get_card($data);
+
+    db_set_title($data["id"], $title);
     
     if ($old["content"] === $content){
         return json_encode(array("success" => "not modified" ));
     }
+    
     $patcher = new diff_match_patch();    
     
     $patch = $patcher->patch_make($old["content"], $content);
