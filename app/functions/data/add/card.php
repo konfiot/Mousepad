c31158c511b911adec859940a5dce46d4b4a76d5
Arthur <spam.00456@gmail.com>
2013-09-26 13:01:22 -0400

    Maintenanat, si la note n'existe pas, le JS la crée

--- app/functions/data/add/card.php
+++ app/functions/data/add/card.php
@@ -7,7 +7,7 @@ include "../functions/diff_match_patch.php";
 function add_card($data) {
     $title = htmlspecialchars($data["title"]);
     $content = htmlspecialchars($data["content"]);
-    $tags = htmlspecialchars($data["tags"]);
+    $tags = $data["tags"];
     
     $patcher = new diff_match_patch();    
     
@@ -23,7 +23,7 @@ function add_card($data) {
     if (db_add_diff($diff, $id) === false){
         return json_encode(array("error" => "Database error" ));
     } else {
-        return json_encode(array("succes" => "note successfully added"));
+        return json_encode(array("id" => $id));
     }
 }
 ?>
