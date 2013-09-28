b40970c3dc07def862985d4666fe28538c8eca8c
Arthur <spam.00456@gmail.com>
2013-09-28 07:37:51 -0400

    Ajout du système de favoris

--- app/functions/data/mod/card.php
+++ app/functions/data/mod/card.php
@@ -3,6 +3,7 @@ include "../functions/data/get.php";
 include "../functions/database/patch/add.php";
 include "../functions/database/card/set_title.php";
 include "../functions/database/card/set_tags.php";
+include "../functions/database/card/set_star.php";
 include_once "../functions/diff_match_patch.php";
 
 
@@ -12,24 +13,45 @@ function mod_card($data) {
     $content = htmlspecialchars($data["content"]);
     $title = htmlspecialchars($data["title"]);
     $tags = $data["tags"];
-    
-    $old = get_card($data);
-    db_set_title($data["id"], $title);
-    db_set_tags($data["id"], $tags);
+    $star = $data["star"];
 
-    if ($old["content"] === $content){
-        return json_encode(array("success" => "not modified" ));
+    if (isset($data["title"])){
+        if(db_set_title($data["id"], $title) === false){
+            return array("error" => "Database error" );
+        }
     }
     
-    $patcher = new diff_match_patch();    
-    
-    $patch = $patcher->patch_make($old["content"], $content);
-    $diff = $patcher->patch_toText($patch);
-
-    if (db_add_diff($diff, $data["id"]) === false){
-        return json_encode(array("error" => "Database error" ));
-    } else {
-        return json_encode(array("succes" => "note successfully modified"));
+    if (isset($data["tags"])){
+        if(db_set_tags($data["id"], $tags) === false){
+            return array("error" => "Database error" );
+        }
     }
+    
+    if (isset($data["star"])){
+        if(db_set_star($data["id"], $star) === false){
+            return array("error" => "Database error");
+        }
+    }
+
+
+    if (isset($data["content"])){
+        $old = get_card($data);
+        
+        if ($old["content"] === $content){
+            return array("success" => "not modified" );
+        }
+        
+        $patcher = new diff_match_patch();    
+        
+        $patch = $patcher->patch_make($old["content"], $content);
+        $diff = $patcher->patch_toText($patch);
+    
+        if (db_add_diff($diff, $data["id"]) === false){
+            return array("error" => "Database error" );
+        }
+    }
+
+    return array("success" => "note successfully modified" );
+
 }
 ?>
