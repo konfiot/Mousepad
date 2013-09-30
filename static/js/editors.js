5434387a332d2bdad263cf3c8e913cde6cb3ae65
Arthur <spam.00456@gmail.com>
2013-09-30 10:04:39 -0400

    recodage du gestionnaire d'éditeur, qui est plus modulable

---
+++ static/js/editors.js
@@ -0,0 +1,50 @@
+function Editor(selector){
+    this.selector = selector;
+}
+
+Editor.prototype.getModes = function(){
+    return this.modes;
+}
+
+function Note(selector){
+    this.selector = selector;
+    this.modes = ["WYSIWYG", "Classic"];
+    this.current_editor = "WYSIWYG";
+}
+
+Note.prototype = Object.create(Editor.prototype);
+
+Note.prototype.setValue = function (value) {
+    $(this.selector).html(marked(value));
+}
+
+Note.prototype.getValue = function(){
+    return (this.current_editor === "WYSIWYG") ? md($(this.selector).html()) : $("#input").val();
+}
+
+Note.prototype.switch = function (editor){
+    if (editor === "WYSIWYG"){
+        if (this.current_editor === "Classic"){
+            this.current_editor = "WYSIWYG";
+            $("#input").unbind();
+            var content = marked($("#input").val());
+            $(this.selector).html(content);
+            this.editor.rebuild();
+        }
+    } else if (editor === "Classic"){
+        if (this.current_editor === "WYSIWYG"){
+            this.current_editor = "Classic";
+            content = md($(this.selector).html());
+            this.editor.destroy();
+            $(this.selector).html("<textarea id='input' rows='" + content.split(/\r\n|\r|\n/).length + "'>" + content + "</textarea>");
+            $(this.selector).css("height", "100%");
+            $("#input").bind('input', function(){
+                $("#input").attr('rows', $("#input").val().split(/\r\n|\r|\n/).length);
+            });
+        }
+    }
+}
+
+Note.prototype.init = function () {
+    this.editor = new Pen(this.selector);
+}
