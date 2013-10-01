c697444a30759ee5409fd0617cdf809f6d777108
Arthur <spam.00456@gmail.com>
2013-10-01 13:52:02 -0400

    Ajout du mode reminder

--- static/js/editors.js
+++ static/js/editors.js
@@ -15,7 +15,7 @@ function Note(selector){
 Note.prototype = Object.create(Editor.prototype);
 
 Note.prototype.setValue = function (value) {
-    $(this.selector).html(marked(value));
+    (this.current_editor === "WYSIWYG") ? $(this.selector).html(marked(value)) : $("#input").val(value);
 }
 
 Note.prototype.getValue = function(){
@@ -47,4 +47,41 @@ Note.prototype.switch = function (editor){
 
 Note.prototype.init = function () {
     this.editor = new Pen(this.selector);
+    $(this.selector).html("Content");
+}
+
+function Reminder(selector){
+    this.selector = selector;
+    this.modes = ["Time", "Place"];
+    this.current_editor = "Time";
+}
+
+Reminder.prototype = Object.create(Editor.prototype);
+
+Reminder.prototype.init = function(){
+    $(this.selector).html('<div class="well"><form><div class="form-group"><label>Date</label><div class=\'input-group date\' id="datetimepicker1"><span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span><input id="calendar" type=\'text\' class="form-control" /></div></div><div class="form-group"><label>Additional Information</label><textarea id="addinfo" style="resize: vertical" class="form-control" rows="3"></textarea></div></form></div>');
+    $('#datetimepicker1').datetimepicker({
+        language: 'fr',
+        pickSeconds: false,
+        maskInput: false
+    });
+}
+
+Reminder.prototype.setValue = function (value) {
+    var data = JSON.parse(value);
+    if (data["type"] === "Time"){
+        this.switch("time");
+        $("#calendar").val(data["date"]);
+        $("#addinfo").val(data["addinfo"]);
+    }
+}
+
+Reminder.prototype.getValue = function () {
+    if (this.current_editor === "Time") {
+        return JSON.stringify({
+            type: "time",
+            date: $("#calendar").val(),
+            addinfo: $("#addinfo").val()
+        });
+    }
 }
