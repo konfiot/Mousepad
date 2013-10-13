ed328f2c2a1ddbaa79b9f2f05d7210449e7838c1
Arthur <spam.00456@gmail.com>
2013-10-13 12:07:14 -0400

    Gestion des dossiers : FAIT

---
+++ app/functions/database/card/set_dir.php
@@ -0,0 +1,34 @@
+<?php
+define(DBTYPE, "json");
+define(JSONFILECARDS, "cards.json");
+
+function db_set_dir($id, $dir){
+    switch (DBTYPE){
+        case "json" :
+            touch(JSONFILECARDS);
+            
+            $file = fopen(JSONFILECARDS, "a+");
+            if(!$file){
+                return false;
+            }
+            while (flock($file, LOCK_EX) === false);
+            $contents = fread($file, filesize(JSONFILECARDS));
+            
+            $json = json_decode($contents, true);
+            
+            $json[$id]["dir"] = $dir;
+
+            ftruncate($file, 0);
+            fwrite($file, json_encode($json));
+            clearstatcache();
+            flock($file, LOCK_UN);
+            fclose($file);
+            
+            return true;
+        break;
+        default :
+            return false;
+        break;
+    }
+}
+?>
