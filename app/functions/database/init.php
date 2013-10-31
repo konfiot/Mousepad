9d7595c03f0cffaf5c44a27503eaac902680e708
Arthur <spam@crana.tk>
2013-10-31 08:14:17 -0400

    Deplacement des fichiers de données vers le dossier /data

--- app/functions/database/init.php
+++ app/functions/database/init.php
@@ -3,7 +3,7 @@ function db_init($dbtype){
     $define = "<?php define('DBTYPE', '$dbtype');";
     switch ($dbtype){
         case 'json' : 
-            $define .= "define('JSONFILECARDS', 'cards.json');define('JSONFILEDIFFS', 'diffs.json');define('JSONFILEUSERS', 'users.json');";
+            $define .= "define('JSONFILECARDS', '../../data/cards.json');define('JSONFILEDIFFS', '../../data/diffs.json');define('JSONFILEUSERS', '../../data/users.json');";
         break;
         default :
             return false;
