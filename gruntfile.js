adea616d5888d091ab8ba84435e4d459c2515731
Arthur <spam@crana.tk>
2014-02-24 11:35:56 -0500

    Réorganisation des dossiers

--- gruntfile.js
+++ gruntfile.js
@@ -17,85 +17,85 @@ module.exports = function(grunt){
 		concat: {
             edit: {
                 src: ["bower_components/jquery/jquery.js", "bower_components/bootstrap/dist/js/bootstrap.js", "bower_components/nprogress/nprogress.js",  "bower_components/typeahead.js/dist/typeahead.js", "bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js", "bower_components/alertify.js/lib/alertify.js", "bower_components/hogan/web/builds/2.0.0/hogan-2.0.0.js", "bower_components/requirejs/require.js", "static/js/editors.js", "static/js/edit.js", "static/js/feedback.js", "static/js/utils.js"],
-                dest: "dist/edit.js"
+                dest: "dist/js/edit.js"
             },
             list: {
                 src: ["bower_components/jquery/jquery.js", "bower_components/bootstrap/dist/js/bootstrap.js", "bower_components/jquery-qrcode/jquery.qrcode.min.js", "bower_components/nprogress/nprogress.js", "bower_components/alertify.js/lib/alertify.js", "bower_components/leaflet-dist/leaflet.js", "bower_components/hogan/web/builds/2.0.0/hogan-2.0.0.js", "static/js/list.js", "static/js/feedback.js", "static/js/utils.js"],
-                dest: "dist/list.js"
+                dest: "dist/js/list.js"
             },
             install: {
                 src: ["bower_components/jquery/jquery.js", "bower_components/bootstrap/dist/js/bootstrap.js", "bower_components/jsTimezoneDetect/jstz.js", "bower_components/zxcvbn/zxcvbn-async.js", "static/js/install.js"],
-                dest: "dist/install.js"
+                dest: "dist/js/install.js"
             },
             login: {
                 src: ["bower_components/jquery/jquery.js", "bower_components/nprogress/nprogress.js", "bower_components/alertify.js/lib/alertify.js", "static/js/login.js", "static/js/feedback.js"],
-                dest: "dist/login.js"
+                dest: "dist/js/login.js"
             },
             settings: {
                 src: ["bower_components/jquery/jquery.js", "bower_components/bootstrap/dist/js/bootstrap.js", "bower_components/nprogress/nprogress.js", "bower_components/alertify.js/lib/alertify.js", "bower_components/jsTimezoneDetect/jstz.js", "bower_components/zxcvbn/zxcvbn-async.js", "bower_components/hogan/web/builds/2.0.0/hogan-2.0.0.js", "static/js/settings.js", "static/js/feedback.js"],
-                dest: "dist/settings.js"
+                dest: "dist/js/settings.js"
             },
             list_css : {
                 src: ["bower_components/font-awesome/css/font-awesome.min.css", "bower_components/bootstrap/dist/css/bootstrap.min.css", "bower_components/nprogress/nprogress.css", "bower_components/alertify.js/themes/alertify.default.css", "bower_components/alertify.js/themes/alertify.core.css", "bower_components/leaflet-dist/leaflet.css", "bower_components/leaflet-dist/leaflet.css"],
-                dest: "dist/list.css"
+                dest: "dist/css/list.css"
             },
             install_css : {
                 src: ["bower_components/font-awesome/css/font-awesome.min.css", "bower_components/bootstrap/dist/css/bootstrap.min.css"],
-                dest: "dist/install.css"
+                dest: "dist/css/install.css"
             },
             login_css : {
                 src: ["bower_components/font-awesome/css/font-awesome.min.css", "bower_components/bootstrap/dist/css/bootstrap.min.css", "bower_components/nprogress/nprogress.css", "bower_components/alertify.js/themes/alertify.default.css", "bower_components/alertify.js/themes/alertify.core.css"],
-                dest: "dist/login.css"
+                dest: "dist/css/login.css"
             },
             settings_css : {
                 src: ["bower_components/font-awesome/css/font-awesome.min.css", "bower_components/bootstrap/dist/css/bootstrap.min.css", "bower_components/nprogress/nprogress.css", "bower_components/alertify.js/themes/alertify.default.css", "bower_components/alertify.js/themes/alertify.core.css"],
-                dest: "dist/settings.css"
+                dest: "dist/css/settings.css"
             },
             edit_css : {
                 src: ["bower_components/font-awesome/css/font-awesome.min.css", "bower_components/bootstrap/dist/css/bootstrap.min.css", "bower_components/nprogress/nprogress.css", "bower_components/alertify.js/themes/alertify.default.css", "bower_components/alertify.js/themes/alertify.core.css", "bower_components/pen/src/pen.css", "bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.css", "bower_components/bootstrap3-datetimepicker/build/css/bootstrap-datetimepicker.min.css", "bower_components/leaflet-dist/leaflet.css", "bower_components/L.GeoSearch/src/css/l.geosearch.css", "bower_components/literallycanvas/lib/css/literally.css", "bower_components/codemirror/lib/codemirror.css"],
-                dest: "dist/edit.css"
+                dest: "dist/css/edit.css"
             },
             pen : {
                 src : ["bower_components/pen/src/pen.js", "bower_components/pen/src/markdown.js"],
-                dest: "dist/vendor/pen.js"
+                dest: "dist/js/vendor/pen.js"
             },
             md : {
                 src : ["bower_components/html-md/lib/md.js"],
-                dest: "dist/vendor/md.js"
+                dest: "dist/js/vendor/md.js"
             },
             marked : {
                 src : ["bower_components/marked/lib/marked.js"],
-                dest: "dist/vendor/marked.js"
+                dest: "dist/js/vendor/marked.js"
             },
             leaflet : {
                 src : ["bower_components/leaflet-dist/leaflet.js", "bower_components/L.GeoSearch/src/js/l.control.geosearch.js", "bower_components/L.GeoSearch/src/js/l.geosearch.provider.openstreetmap.js"],
-                dest: "dist/vendor/leaflet.js"
+                dest: "dist/js/vendor/leaflet.js"
             },
             datetimepicker : {
                 src : ["bower_components/bootstrap3-datetimepicker/build/js/bootstrap-datetimepicker.min.js"],
-                dest: "dist/vendor/datetime.js"
+                dest: "dist/js/vendor/datetime.js"
             },
             literallycanvas : {
                 src : ["bower_components/literallycanvas/lib/js/literallycanvas.jquery.js"],
-                dest: "dist/vendor/literallycanvas.js"
+                dest: "dist/js/vendor/literallycanvas.js"
             },
             codemirror : {
                 src : ["bower_components/codemirror/lib/codemirror.js", "bower_components/codemirror/keymap/emacs.js", "bower_components/codemirror/keymap/vim.js"],
-                dest: "dist/vendor/codemirror.js"
+                dest: "dist/js/vendor/codemirror.js"
             }
 		},
 		uglify : { 
             main : {
                 expand: true,
-                cwd: "dist",
+                cwd: "dist/js",
                 src : "*.js",
-                dest: "dist",
+                dest: "dist/js",
             },
             vendor : {
                 expand: true,
-                cwd: "dist/vendor/",
+                cwd: "dist/js/vendor/",
                 src : "*.js",
-                dest: "dist/vendor/",
+                dest: "dist/js/vendor/",
             },
             templates : {
                 expand: true,
@@ -104,12 +104,21 @@ module.exports = function(grunt){
                 dest: "static/themes/",
             }
 		},
+		copy: {
+		    main: {
+		        expand: true,
+                cwd: "bower_components/font-awesome/fonts/",
+                src: '*',
+                dest: 'dist/fonts/',
+		    }
+		}
 	});
 	
 	grunt.loadNpmTasks('grunt-hogan');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('default', ['hogan', 'concat', 'uglify']);
-	grunt.registerTask('dev', ['hogan', 'concat']);
+	grunt.registerTask('default', ['hogan', 'concat', 'copy', 'uglify']);
+	grunt.registerTask('dev', ['hogan', 'concat', 'copy']);
 };
